ee2decc32f07cc78a094913b03489cd3e05a0080
Mandi <haase1020@gmail.com>
2021-01-24 19:02:22 -0500

    finish app

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
+const dbURI =
+  " mongodb+srv://haase1020:password1234@test.cm24m.mongodb.net/test?retryWrites=true&w=majority";
+const mongoose = require("mongoose");
+
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+//   .then((result) => app.listen(3000))
+//   .catch((err) => console.log("houston, there is a problem", err));
+
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to database"));
+
+app.use(express.json());
+
+const subscribersRouter = require("./routes/subscribers");
+
+app.use("/subscribers", subscribersRouter);
+("loca");
 
 app.listen(3000, () => {
   console.log("server is running");
